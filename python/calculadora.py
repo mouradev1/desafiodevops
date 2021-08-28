9341a6a9153ac7aaa5cd84edfac86d43e0c267d0
Moura <mouralol23@gmailcom>
2021-08-28 19:58:12 -0300

    Ultima verificaçao na calculadora python

--- python/calculadora.py
+++ python/calculadora.py
@@ -1,12 +1,18 @@
-
+import sys
 def log(log):
     f = open("./log/calculadorapython.log", "w")
     f.write(log + " \n")
     f.close()
-def calculadora():
-    A = float(input("Digite um inteiro: "))
-    B = float(input("Digite outro inteiro: "))
-    resultado = A / B
+def calculadora(A,B):
+    resultado = (A / B)
     print('Resultado da soma {:.1f}'.format(resultado))
     log('Resultado da soma {:.1f}'.format(resultado))
-calculadora()
+
+if len(sys.argv) > 1:
+    A = float(sys.argv[1]);
+    B =float(sys.argv[2])
+    calculadora(A,B)
+else:
+    A = float(input("Digite um inteiro: "))
+    B = float(input("Digite outro inteiro: "))
+    calculadora(A,B)

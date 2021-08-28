460da00f34e687e8fb92b9deac609a8d7c3dc02b
Moura <mouralol23@gmailcom>
2021-08-27 21:35:20 -0300

    Criando o menu por projeto

---
+++ shell/main.sh
@@ -0,0 +1,12 @@
+title="MENU DESAFIO DEVOPS" 
+menu=("PING" "CALCULADORA") ##MENU
+
+while i=$(zenity --title="$title" --list \
+                 --column="SELECIONE UMA OPÇÃO" "${menu[@]}" --width="350" --height="250")
+do
+    case "$i" in
+    "${menu[0]}")./calculadora.sh;;
+    "${menu[1]}")./ping_test.sh;;
+    *) zenity --error --text="Opção Invalida";;
+    esac
+done

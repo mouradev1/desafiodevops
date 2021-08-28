f0bad7aa8d9434d6131d00b088213bed332acf0f
Moura <mouralol23@gmailcom>
2021-08-27 21:37:54 -0300

    Corrigindo main

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ while i=$(zenity --title="$title" --list \
 do
     case "$i" in
     "${menu[0]}")./calculadora.sh;;
-    "${menu[1]}")./ping_test.sh;;
+    "${menu[1]}")./ping.sh;;
     *) zenity --error --text="Opção Invalida";;
     esac
 done

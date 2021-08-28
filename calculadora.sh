a3b4d18d404ae288736cff2ee1114226f1f5b9f2
Moura <mouralol23@gmailcom>
2021-08-28 11:11:44 -0300

    Adicionando script de soma e validando logs

--- calculadora.sh
+++ calculadora.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+#Moura dev calculadora 1.0
+log=./log/calculadora.log
+if [ $1 != null ]; then
+   echo "##### RESULTADO  #####"
+   echo " $1 / $2"
+   echo "scale=1; $1 / $2" | bc
+   echo -e "Resultado da soma via parametro: $(echo "scale=1; $1 / $2" | bc) "${USER}  "DATA: " `date +%d/%m/%Y" HORA: "%H:%M:%S`   >> $log
+else
+A=$(zenity --title="valor inicial: " --text "Digite um numero " --entry --width="250" height="100")
+B=$(zenity --title="Segundo  numero" --text "Digite o segundo  numero?" --entry --width="250" height="100")
+
+echo -e "Resultado da soma: $(echo "scale=1; $A / $B" | bc) USER:  "${USER}  "DATA: " `date +%d/%m/%Y" HORA: "%H:%M:%S`   >> $log
+zenity --info --title="INFORMAÇÃO" --text="Resultado: $(echo "scale=1; $A / $B" | bc)" --width="200" height="100"
+fi
--- main.sh
+++ main.sh
@@ -6,8 +6,8 @@ while i=$(zenity --title="$title" --list \
                  --column="SELECIONE UMA OPÇÃO" "${menu[@]}" --width="350" --height="250")
 do
     case "$i" in
-    "${menu[0]}")./shell/ping/ping.sh;;
-    "${menu[1]}")./shell/calculadora/calculadora.sh;;
+    "${menu[0]}")./ping.sh;;
+    "${menu[1]}")./calculadora.sh;;
     *) zenity --error --text="Opção Invalida";;
     esac
 done

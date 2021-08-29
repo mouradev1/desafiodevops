ac457ef204d69a989a248f0779a9d80937917638
Moura <mouralol23@gmailcom>
2021-08-29 11:00:29 -0300

    Subindo diagrama

--- python/ping.py
+++ python/ping.py
@@ -1,14 +1,11 @@
 import os
 import sys
-
-
 def validaPing(ip):
     result = os.system("ping -c 1 " + ip)
     if result == 0:
         print('\033[32m'+'LINK UP '+'\033[0;0m')
     else:
         print('\033[31m'+'LINK DOWN '+'\033[0;0m')
-
 
 if len(sys.argv) > 1:
     validaPing(sys.argv[1])
